f8706914eb94e39abcec13ed01611237853cc437
David Daza <drdaza18@gmail.com>
2023-01-09 11:30:16 +0100

    make test kata FizzBuzz

--- src/main/java/com/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/fizzbuzz/FizzBuzz.java
@@ -1,5 +1,35 @@
 package com.fizzbuzz;
 
 public class FizzBuzz {
+    private Integer TestNumber = 0;
+
+    public FizzBuzz(Integer testNumber) {
+        this.TestNumber = testNumber;
+    }
+
+    public Integer getTestNumber() {
+        return TestNumber;
+    }
+
+    public void setTestNumber(Integer testNumber) {
+        this.TestNumber = testNumber;
+    }
     
+    public String TestFizzBuzz(Integer numberToTest){
+        if(numberToTest % 3 == 0 && numberToTest % 5 == 0) return "FizzBuzz";
+        if(numberToTest % 3 == 0 || verifyIncludeNumber(numberToTest) == "three") return "Fizz";
+        if(numberToTest % 5 == 0 || verifyIncludeNumber(numberToTest) == "five") return "Buzz";
+        
+        String numberCast= Integer.toString(numberToTest);
+        return numberCast;
+    }
+    public String verifyIncludeNumber(Integer testNumber){
+        String numberCast = Integer.toString(testNumber);
+
+        if(numberCast.contains("3")) return "three";
+
+        if(numberCast.contains("5")) return "five";
+
+        return numberCast;
+    }
 }
--- src/test/java/com/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/fizzbuzz/FizzBuzzTest.java
@@ -1,14 +1,22 @@
 package com.fizzbuzz;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class FizzBuzzTest {
     
     @Test
     public void test_fizzbuzz_example() {
         assertEquals(2, 2);
+    }
+    @Test
+    public void test_fizzbuzz_with_number(){
+        FizzBuzz myFizzBuzz = new FizzBuzz(52);
+
+        String respuest = myFizzBuzz.TestFizzBuzz(myFizzBuzz.getTestNumber());
+
+        assertEquals("Buzz", respuest);
     }
 
 }
